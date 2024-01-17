cf234535c130adc93204d935c4b2be27fa9cde7a
DevAcrip <jazapata.adm@gmail.com>
2024-01-16 23:57:08 -0500

    Render list added

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -1,21 +1,43 @@
 import "./App.css";
 import TwitterFollowCard from "./TwitterFollowCard";
 
+const users = [
+  {
+    userName: "midudev",
+    name: "Miguel Angel Duran",
+    isFollowing: true,
+  },
+  {
+    userName: "DevAcrip",
+    name: "Jorge Andres Zapata",
+    isFollowing: true,
+  },
+  {
+    userName: "pheralb",
+    name: "Pablo Hernandez",
+    isFollowing: false,
+  },
+  {
+    userName: "vizapata",
+    name: "Victor Alfonso Zapata",
+    isFollowing: true,
+  },
+];
+
 export function App() {
   return (
     <section className="App">
-      <TwitterFollowCard initialIsFollowing userName="DevAcrip">
-        Jorge Andrés Zapata
-      </TwitterFollowCard>
-
-      <TwitterFollowCard initialIsFollowing={false} userName="Midudev">
-      Miguel Angel Duran
-      </TwitterFollowCard>
-
-      <TwitterFollowCard initialIsFollowing userName="Vizapata">
-      Victor Alfonso Zapata
-      </TwitterFollowCard>
-      
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
     </section>
   );
 }

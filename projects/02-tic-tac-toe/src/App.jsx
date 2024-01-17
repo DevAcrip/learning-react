4c73cddff58c641115bfceb1601beeb0df8945a6
DevAcrip <jazapata.adm@gmail.com>
2024-01-17 11:48:16 -0500

    State with player turn

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 
 const TURNS = {
@@ -5,17 +6,20 @@ const TURNS = {
   O: 'o'
 }
 
-const board = Array(9).fill(null)
 
-const Square = ({children, updateBoard, index}) => {
+
+const Square = ({children, isSelected, updateBoard, index}) => {
+  const className = `square ${isSelected ? 'is-selected':''}`
   return (
-    <div className='square'>
+    <div className={className}>
       {children}
     </div>
   )
 }
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
   return (
     <main className='board'>
       <h1>Tic tac toe</h1>
@@ -27,11 +31,15 @@ function App() {
                 key={index}
                 index={index}
               >
-                {index}
+                {board[index]}
               </Square>
             )
           })
         }
+      </section>
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
     </main>
   )

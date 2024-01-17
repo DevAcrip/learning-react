28c18c40f7cfe944d20e3ef1342e937ddbda44f4
DevAcrip <jazapata.adm@gmail.com>
2024-01-16 23:43:35 -0500

    Follow and unfollow effect hover on button

--- projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
+++ projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
@@ -13,21 +13,24 @@ export function TwitterFollowCard({ children, userName = 'unknown', initialIsFol
     setIsFollowing(!isFollowing)
   }
   return (
-    <article className="tw-followCard">
-      <header className="tw-followCard-header">
+    <article className='tw-followCard'>
+      <header className='tw-followCard-header'>
         <img
-          className="tw-followCard-avatar"
+          className='tw-followCard-avatar'
           src={`https://unavatar.io/${userName}`}
-          alt="El avatar de acrip"
+          alt='El avatar de acrip'
         />
-        <div className="tw-followCard-info">
+        <div className='tw-followCard-info'>
           <strong>{children}</strong>
-          <span className="tw-followCard-infoUsername">@{userName}</span>
+          <span className='tw-followCard-infoUsername'>@{userName}</span>
         </div>
       </header>
 
       <aside>
-        <button className={buttonClassName} onClick={handleClick}>{text}</button>
+        <button className={buttonClassName} onClick={handleClick}>
+          <span className='tw-followCard-text'>{text}</span>
+          <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
+        </button>
       </aside>
     </article>
   )

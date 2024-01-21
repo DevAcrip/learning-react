be9ade09420e832c62cf2b7b55cb096e8831d3a9
DevAcrip <jazapata.adm@gmail.com>
2024-01-20 23:51:58 -0500

    Added resetGameStorage

--- projects/02-tic-tac-toe/src/logic/storage/index.js
+++ projects/02-tic-tac-toe/src/logic/storage/index.js
@@ -1,4 +1,9 @@
-export function saveGameToStorage = ({board, turn}) => {
+export const saveGameToStorage = ({board, turn}) => {
     window.localStorage.setItem('board', JSON.stringify(board))
     window.localStorage.setItem('turn', turn)
+}
+
+export const resetGameStorage = () => {
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
 }

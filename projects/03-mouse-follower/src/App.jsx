016e7165a50ac165cb1518311e85d3d156978fd8
DevAcrip <jazapata.adm@gmail.com>
2024-01-21 23:35:12 -0500

    mouse follower state config

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,6 +1,11 @@
-import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [enabled, setEnabled] = useState(false)
+  useEffect(() => {
+    console.log("Efecto");
+  })
 
   return <h1>Proyecto 3</h1>;
 }

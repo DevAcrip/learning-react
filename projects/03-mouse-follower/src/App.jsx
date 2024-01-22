c5e71da0c0fbf5fb7a6e1efa87428cef49ff3a03
DevAcrip <jazapata.adm@gmail.com>
2024-01-22 00:14:59 -0500

    useEffect return fuction added, running

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [enabled, setEnabled] = useState(false);
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({x: 0, y: 0})
   useEffect(() => {
     console.log("Effect", {enabled});
     const handleMove = (event) => {
       const {clientX, clientY } = event
       console.log('handleMove', {clientX, clientY});
+      setPosition({x: clientX, y: clientY})
     }
     if (enabled){
       window.addEventListener('pointermove', handleMove)
     }
+    return () => {window.removeEventListener('pointermove', handleMove)}
   }, [enabled]);
 
   return (
@@ -25,7 +28,7 @@ function App() {
         top: -20,
         width: 40,
         height: 40,
-        transform: 'translate(0px, 0px)'
+        transform: `translate(${position.x}px, ${position.y}px)`
       }}/>
       <button onClick={() => setEnabled(!enabled)}>{enabled ? "Desactivar" : "Activar"} seguir puntero</button>
     </main>

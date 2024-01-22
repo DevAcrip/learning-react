d7c25e91a45a62023b0ed01f3297e45425459199
DevAcrip <jazapata.adm@gmail.com>
2024-01-21 23:37:05 -0500

    Button enable mouse follower

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
 
 function App() {
-
-  const [enabled, setEnabled] = useState(false)
+  const [enabled, setEnabled] = useState(false);
   useEffect(() => {
     console.log("Efecto");
-  })
+  });
 
-  return <h1>Proyecto 3</h1>;
+  return (
+    <>
+      <h1>Proyecto 3</h1>
+      <button>Activar seguir puntero</button>
+    </>
+  );
 }
-
 export default App;

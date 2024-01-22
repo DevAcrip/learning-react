871d10660e57b3897c0473c1b7f9fc9c9015a960
DevAcrip <jazapata.adm@gmail.com>
2024-01-21 23:55:55 -0500

    event listener of the pointerMove

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -3,14 +3,32 @@ import { useEffect, useState } from "react";
 function App() {
   const [enabled, setEnabled] = useState(false);
   useEffect(() => {
-    console.log("Efecto");
-  });
+    console.log("Effect", {enabled});
+    const handleMove = (event) => {
+      const {clientX, clientY } = event
+      console.log('handleMove', {clientX, clientY});
+    }
+    if (enabled){
+      window.addEventListener('pointermove', handleMove)
+    }
+  }, [enabled]);
 
   return (
-    <>
-      <h1>Proyecto 3</h1>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: 'translate(0px, 0px)'
+      }}/>
       <button onClick={() => setEnabled(!enabled)}>{enabled ? "Desactivar" : "Activar"} seguir puntero</button>
-    </>
+    </main>
   );
 }
 export default App;
